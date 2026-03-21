44f928506c41a0367072f4e6d2e3cd24ccaf5421
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-21 16:41:36 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ import {
 const Contact = () => {
   const [map, setMap] = useState("");
   const [images, setImages] = useState([]);
-  const BASE_URL = "http://127.0.0.1:8000";
+  const BASE_URL = "https://vts-backend-wky4.onrender.com";
   const navigate = useNavigate();
   const [form, setForm] = useState({
     name: "",
